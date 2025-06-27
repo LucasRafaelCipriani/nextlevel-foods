fd1563f67a6cce5dbf06c6a6f91a5e78ce919441
Lucas Cipriani <lcipriane@mcfadyen.com>
2025-06-27 16:35:45 -0300

    Meals pages populated with DB data

---
+++ app/meals/[slug]/loading.tsx
@@ -0,0 +1,7 @@
+import classes from './loading.module.css';
+
+const MealLoading: React.FC = () => {
+  return <p className={classes.loading}>Fetching Meal Details...</p>;
+};
+
+export default MealLoading;
--- lib/meals.ts
+++ lib/meals.ts
@@ -11,3 +11,12 @@ export const getMeals = async (): Promise<Meal[]> => {
     throw new Error('Failed to fetch meal data. Please try again later.');
   }
 };
+
+export const getMeal = async (slug: string): Promise<Meal> => {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug) as Meal;
+  } catch {
+    throw new Error('Failed to fetch meal data. Please try again later.');
+  }
+};
